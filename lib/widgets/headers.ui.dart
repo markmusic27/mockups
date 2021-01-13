6d0ddf818cbb38a03dfb127905dad300dd720baa
Mark Music <markmusic999@gmail.com>
2021-01-13 14:20:33 -0600

    ft[icons] => 🐛 Add octocat and fullscreen icons to pubspec.yaml and widgets folder

--- lib/widgets/headers.ui.dart
+++ lib/widgets/headers.ui.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:mockups/widgets/topButtons.ui.dart';
 
 class Headers extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.red,
+      margin: EdgeInsets.all(20),
       height: 250,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          TopButtons(),
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.green,
+          ),
+          SizedBox(),
+        ],
+      ),
     );
   }
 }
---
+++ lib/widgets/topButtons.ui.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class TopButtons extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: Row(
+        children: [
+          Container(
+            child: Row(
+              children: [],
+            ),
+          ),
+          SizedBox(width: 20),
+        ],
+      ),
+    );
+  }
+}
