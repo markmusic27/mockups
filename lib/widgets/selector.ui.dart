c2362dee5d315c270e6da5b1a93ca42ec145d8c0
Mark Music <markmusic999@gmail.com>
2021-01-15 17:36:20 -0600

    ft[SwiperArrows] => 🐸 Add IconData to swiper

--- lib/widgets/selector.ui.dart
+++ lib/widgets/selector.ui.dart
@@ -10,7 +10,10 @@ class Selector extends StatelessWidget {
         padding: EdgeInsets.only(bottom: 20),
         child: Swiper(
           control: SwiperControl(
-            onTap: () {},
+            size: 30,
+            iconNext: Icons.arrow_forward_ios_rounded,
+            iconPrevious: Icons.arrow_back_ios_rounded,
+            padding: EdgeInsets.all(30),
             color: Colors.white,
           ),
           itemBuilder: (BuildContext context, int i) =>
