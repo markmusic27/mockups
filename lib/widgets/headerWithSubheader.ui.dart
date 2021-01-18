90a56d957a4d5a1ef781e0ca6e59077512492927
Mark Music <markmusic999@gmail.com>
2021-01-17 20:54:03 -0600

    ft[text] => 🧜‍♀️ Remove text gradient because of build faliure

--- lib/widgets/headerWithSubheader.ui.dart
+++ lib/widgets/headerWithSubheader.ui.dart
@@ -1,78 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:gradient_ui_widgets/gradient_ui_widgets.dart';
-import 'package:mockups/services/mapGradient.service.dart';
-import 'package:mockups/state/imageCount.store.dart';
+import 'package:mockups/utils/constants.dart';
 import 'package:mockups/widgets/subheader.ui.dart';
-import 'package:provider/provider.dart';
 
 class HeaderWithSubheader extends StatefulWidget {
   @override
   _HeaderWithSubheaderState createState() => _HeaderWithSubheaderState();
 }
 
-class _HeaderWithSubheaderState extends State<HeaderWithSubheader>
-    with TickerProviderStateMixin {
-  AnimationController controller;
-  Animation animation1;
-  Animation animation2;
-  int pointer = 0;
-
-  List<Color> colors = MapGradient.generateColors(1);
-  void animate(int currentImage) {
-    List<Color> fromPointer = MapGradient.generateColors(currentImage);
-    List<Color> toPointer = MapGradient.generateColors(currentImage + 1);
-
-    controller = AnimationController(
-      vsync: this,
-      duration: Duration(milliseconds: 200),
-    );
-
-    animation1 = ColorTween(begin: fromPointer[0], end: toPointer[0])
-        .animate(controller);
-
-    animation2 = ColorTween(begin: fromPointer[1], end: toPointer[1])
-        .animate(controller);
-
-    controller.forward(from: 0);
-
-    controller.addListener(() {
-      setState(() {
-        colors = <Color>[animation1.value, animation2.value];
-      });
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
+class _HeaderWithSubheaderState extends State<HeaderWithSubheader> {
   @override
   Widget build(BuildContext context) {
-    final imageCountStore = Provider.of<ImageCountStore>(context);
     return Container(
       child: Column(
         children: [
-          Observer(
-            builder: (_) {
-              if (pointer != imageCountStore.imageCount) {
-                animate(imageCountStore.imageCount);
-              }
-
-              pointer = imageCountStore.imageCount;
-
-              return GradientText(
-                'mockups.li',
-                gradient: LinearGradient(colors: colors),
-              );
-            },
+          Text(
+            'mockups.li',
+            style: TextStyle(
+              fontFamily: 'Inter',
+              color: kTextColor,
+              fontSize: 60,
+              fontWeight: FontWeight.w800,
+            ),
           ),
           SizedBox(height: 20),
           Subheader(),
--- lib/widgets/subheader.ui.dart
+++ lib/widgets/subheader.ui.dart
@@ -1,83 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:mockups/services/mapGradient.service.dart';
-import 'package:mockups/state/imageCount.store.dart';
 import 'package:mockups/utils/constants.dart';
-import 'package:provider/provider.dart';
 
 class Subheader extends StatefulWidget {
   @override
   _SubheaderState createState() => _SubheaderState();
 }
 
-class _SubheaderState extends State<Subheader> with TickerProviderStateMixin {
-  AnimationController controller;
-  Animation animation1;
-  Animation animation2;
-  int pointer = 0;
-  List<Color> colors = MapGradient.generateColors(1);
-
-  void animate(int currentImage) {
-    List<Color> fromPointer = MapGradient.generateColors(currentImage);
-    List<Color> toPointer = MapGradient.generateColors(currentImage + 1);
-
-    controller = AnimationController(
-      vsync: this,
-      duration: Duration(milliseconds: 200),
-    );
-
-    animation1 = ColorTween(begin: fromPointer[0], end: toPointer[0])
-        .animate(controller);
-
-    animation2 = ColorTween(begin: fromPointer[1], end: toPointer[1])
-        .animate(controller);
-
-    controller.forward(from: 0);
-
-    controller.addListener(() {
-      setState(() {
-        colors = <Color>[animation1.value, animation2.value];
-      });
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
+class _SubheaderState extends State<Subheader> {
   @override
   Widget build(BuildContext context) {
-    final imageCountStore = Provider.of<ImageCountStore>(context);
     return Row(
       textBaseline: TextBaseline.alphabetic,
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.baseline,
       children: [
-        SelectableText('An online repository of ', style: kSubheader),
-        Observer(
-          builder: (_) {
-            if (pointer != imageCountStore.imageCount) {
-              animate(imageCountStore.imageCount);
-            }
-
-            pointer = imageCountStore.imageCount;
-
-            return SelectableText(
-              '4k',
-              style: kSubheader.copyWith(
-                color: colors[1],
-              ),
-            );
-          },
+        SelectableText(
+          'An online repository of 4k mockups.',
+          style: kSubheader.copyWith(
+            color: kTextColor.withOpacity(0.8),
+          ),
         ),
-        SelectableText(' device mockups.', style: kSubheader),
       ],
     );
   }
